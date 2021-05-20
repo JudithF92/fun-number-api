34948f1f21b0e492dd4337961251810b11732c5b
Judith Feltl <judith.feltl@live.at>
2021-05-20 21:59:44 +0200

    added fetch

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('success');
+})
+
+app.post('/fact', (req, res) => {
+	const url = 'http://numbersapi.com/' + req.body.input.trim() + '?json';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => res.json(data)) 
+      .catch(err => res.status(400).json('unable to work with API'))
+})
+
+
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
